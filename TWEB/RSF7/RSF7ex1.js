9988107a826e3588a1447d5f2af7ecd2b858998a
Miguyy <miguelangelo123machado@gmail.com>
2025-01-06 15:59:48 +0000

    exercises

---
+++ TWEB/RSF7/RSF7ex1.js
@@ -0,0 +1,10 @@
+const num = parseInt(prompt("Indique um número inteiro: "))
+    if (num>0){
+        console.log("É positivo")
+    }
+    if (num<0){
+        console.log("É negtivo")
+    }
+    if (num==0){
+        console.log("É igual a zero")
+    }
