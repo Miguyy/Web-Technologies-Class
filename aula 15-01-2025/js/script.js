99a8a4e1e534cfa807de43c6e6af66a8a3c52398
Miguyy <miguelangelo123machado@gmail.com>
2025-01-31 12:53:43 +0000

    aula 15-01-2025

---
+++ aula 15-01-2025/js/script.js
@@ -0,0 +1,33 @@
+let first_name="miguel";
+const LIMIT=10;
+
+name="angelo";
+num=20
+
+const users=[
+    {
+        id:1,
+        name:"Miguel"
+    },
+    {
+        id:2,
+        name:"angelo"
+    },
+    {
+        id:3,
+        name:"atão"
+    }
+    
+];
+
+/* for (let index=0; index=users.length; index++){
+    const element=users[index];
+    console.log(element. name)
+} */
+
+function multiply(){
+    console.log(2*10);
+}
+multiply()
+
+
